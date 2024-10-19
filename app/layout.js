6cd2ad19cb8dff055763eace80520595652668b9
MuhammedBaki99 <122742218+MuhammedBaki99@users.noreply.github.com>
2024-10-19 13:43:18 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,11 +9,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={leaguespartan.className}>
-        <Header />
     <html lang="tr">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={leaguespartan.className}>
+        <Header /> 
         {children}
       </body>
     </html>
