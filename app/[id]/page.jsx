1ae0db318a751333e8b3e5be32696cf16b4beca8
Muhammed Baki <muhammedbakicaglayan@gmail.com>
2024-10-20 20:52:40 +0300

    detay sayfasına parametre gönderildi ve karşlılaştırma yapıldı id si eşit olan veri ekrana basıldı

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -3,7 +3,25 @@ import EditInvoices from "../../components/editinvoices/editinvoices"
 import "./invoicesdetail.css"
 import Image from "next/image"
 
-export default function InvoicesDetail() {
+export default async function InvoicesDetail({ params }) {
+
+  const response = await fetch("https://invoiceapi.senihay.com/api/Invoice/GetInvoices", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "text/plain",
+      "Cache-Control": "no-cache", // Cache'yi devre dışı bırak
+    },
+    cache: "no-store", // fetch API ile cache'yi kapat
+  })
+  const data = await response.json();
+  console.log(data.length)
+  console.log(typeof params.id);
+
+
+  const filteredData = data.find(x => x.id === Number(params.id));
+  console.log(filteredData, "assdasd");
+
 
   return (
     <div className="invoiceDetail">
@@ -21,34 +39,34 @@ export default function InvoicesDetail() {
           <button className="save">Ödendi olarak işaretle</button>
         </div>
       </div>
-      <div className="detailContent">
+      <div className="detailContent" >
         <div className="dcTop">
           <div className="dctLeft">
-            <p className="dctlTop>"><h4>#XM9141</h4></p>
-            <p className="dctlBottom">Grafik Tasarım</p>
+            <p className="dctlTop>"><h4>#{filteredData.referanceNumber}</h4></p>
+            <p className="dctlBottom">{filteredData.description}</p>
           </div>
           <div className="dctRight">
-            <p> 19 Union Terrace <br /> London <br /> E1 3EZ <br /> United Kingdom</p>
+            <p> {filteredData.fromStreet} <br /> {filteredData.fromCity} <br /> {filteredData.fromPostCode}<br /> {filteredData.fromCountry}</p>
           </div>
         </div>
         <div className="dcMedium">
           <div className="dcmLeft">
             <div>
               <p>Fatura Tarihi</p> <br />
-              <h3>21 Ağustos 2021</h3>
+              <h3>{filteredData.paymentDue}</h3>
             </div>
             <div>
               <p>Vadesi Gelen Ödeme</p>
               <br />
-              <h3>20 Eylül 2021</h3>
+              <h3>{filteredData.invoiceDate}</h3>
             </div>
           </div>
           <div className="dcmMedium">
             <p>fatura</p> <br />
-            <h3>Alex Grim</h3> <br />
+            <h3>{filteredData.clientName}</h3> <br />
             <p>
-              84 Church Way <br /> Bradford <br /> BD1 9PB <br />
-              United Kingdom
+              {filteredData.fromStreet} <br /> {filteredData.fromCity}  <br /> {filteredData.fromPostCode} <br />
+              {filteredData.fromCountry}
             </p>
           </div>
           <div className="dcmRight">
@@ -56,26 +74,29 @@ export default function InvoicesDetail() {
             <h3>alexgrim@mail.com</h3>
           </div>
         </div>
-        <div className="itemsContainer">
-          <div className="column">
-            <p>Ürün Adı</p>
-            <h3 >Afiş Tasarımı</h3>
+        {filteredData ? filteredData.items.map((x, i) => 
+
+          <div className="itemsContainer" key={i}>
+            <div className="column">
+              <p>Ürün Adı</p>
+              <h3 >{x.name}</h3>
+            </div>
+            <div className="itemsDetail">
+              <div className="column">
+                <p>Adet</p>
+                <p>{x.quantity}</p>
+              </div>
+              <div className="column">
+                <p>Fiyat</p>
+                <p>{x.price}</p>
+              </div>
+              <div className="column">
+                <p>Toplam</p>
+                <h3 >{x.price * x.quantity}</h3>
+              </div>
+            </div>
           </div>
-          <div className="itemsDetail">
-            <div className="column">
-              <p>Adet</p>
-              <p>1</p>
-            </div>
-            <div className="column">
-              <p>Fiyat</p>
-              <p>€156.00</p>
-            </div>
-            <div className="column">
-              <p>Toplam</p>
-              <h3 >€156.00</h3>
-            </div>
-          </div>
-        </div>
+        ) : ""}
         <div className="dcFooter">
           <h2>Ödenmesi Gereken Tutar</h2>
           <h2>€556.00</h2>
