07da96710043fe6ac0f6b812fe7436bd3033ce00
Muhammed Baki <muhammedbakicaglayan@gmail.com>
2024-10-20 12:26:02 +0300

    formların tasarımında gerekli düzenlemelr yapıldı

--- app/newinvoices/newinvoices.jsx
+++ app/newinvoices/newinvoices.jsx
@@ -1,27 +1,14 @@
 "use client"
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import "./newinvoices.css"
 import Image from "next/image";
 import { TopPlus } from "../invoicestop/topsvg";
-
+ 
 export default function NewInvoices() {
   const [itemList, setItemList] = useState([]);
   const [additem, setAddItem] = useState(0);
   const [selectedIndex, setSelectedIndex] = useState(0);
-
-  const dialogRef = useRef({});
-
-  function handleClick() {
-    if (dialogRef.current) {
-      dialogRef.current.showModal();
-    }
-  }
-
-  function close() {
-    if (dialogRef.current) {
-      dialogRef.current.close();
-    }
-  }
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     setItemList([...itemList, {
@@ -42,106 +29,113 @@ export default function NewInvoices() {
 
   return (
     <>
-      <button className="newInvoicesBtn"  onClick={() => handleClick()}>
+      <button className="newInvoicesBtn" onClick={() => setShow(true)}>
         <TopPlus />
-        <h2>Yeni Fatura</h2>
-      </button> 
-      <dialog ref={(e) => (dialogRef.current = e)}>
+        <h2 >Yeni Fatura</h2>
+      </button>
 
-        <div className="newInvoicesForm">
-          <h1>Yeni Fatura</h1>
-          <form >
-            <div className="formsections">
-              <div className="formsectionRow">
-                <h4>Bill From</h4>
-                <label htmlFor="streetadress">Sokak Adresi
-                  <input type="text" name="streetadress" defaultValue={"19 Union Terrace"} />
+      <div className="newInvoicesForm" style={{
+        width: show ? "40%" : "0%",
+        transition: "all .3s",
+        padding: show ? "20px 56px" : "0",
+        opacity: show ? "1" : "0"
+      }}>
+        <h1 style={{
+          display: show ? "flex" : "none"
+        }}>Yeni Fatura</h1>
+        <form >
+          <div className="formsections">
+            <div className="formsectionRow">
+              <h4>Bill From</h4>
+              <label htmlFor="streetadress">Sokak Adresi
+                <input type="text" name="streetadress" defaultValue={"19 Union Terrace"} />
+              </label>
+              <div className="citypostcountry">
+                <label htmlFor="city">Şehir
+                  <input type="text" name="citypostcountry" defaultValue={"London"} />
                 </label>
-                <div className="citypostcountry">
-                  <label htmlFor="city">Şehir
-                    <input type="text" name="citypostcountry" defaultValue={"London"} />
-                  </label>
-                  <label htmlFor="postcode">Posta Kodu
-                    <input type="text" name="postcode" defaultValue={"E1 3EZ"} />
-                  </label>
-                  <label htmlFor="country">Ülke
-                    <input type="text" name="country" defaultValue={"United Kingdom"} />
-                  </label>
-                </div>
-              </div>
-
-              <div className="formsectionRow">
-                <h4>Bill To</h4>
-                <label htmlFor="clientsname">Müşterinin Adı
-                  <input type="text" name="clientsname" defaultValue={"19 Union Terrace"} />
+                <label htmlFor="postcode">Posta Kodu
+                  <input type="text" name="postcode" defaultValue={"E1 3EZ"} />
                 </label>
-                <label htmlFor="clientemail">Müşterinin Epostası
-                  <input type="text" name="clientemail" defaultValue={"London"} />
+                <label htmlFor="country">Ülke
+                  <input type="text" name="country" defaultValue={"United Kingdom"} />
                 </label>
-                <label htmlFor="streetadress">Sokak Adresi
-                  <input type="text" name="streetadress" defaultValue={"E1 3EZ"} />
-                </label>
-                <div className="citypostcountry">
-                  <label htmlFor="city">Şehir
-                    <input type="text" name="citypostcountry" defaultValue={"London"} />
-                  </label>
-                  <label htmlFor="postcode">Posta Kodu
-                    <input type="text" name="postcode" defaultValue={"E1 3EZ"} />
-                  </label>
-                  <label htmlFor="country">Ülke
-                    <input type="text" name="country" defaultValue={"United Kingdom"} />
-                  </label>
-                </div>
-                <div className="dateterms">
-                  <label htmlFor="invoicedate">
-                    <input type="date" name="invoicedate" />
-                  </label>
-                  <label htmlFor="invoiceterm">
-                    <select name="invoiceterm">
-                      <option value="Net 1 Gün" selected>Net 1 Gün</option>
-                      <option value="Net 7 Gün" >Net 7 Gün</option>
-                      <option value="Net 14 Gün" >Net 14 Gün</option>
-                      <option value="Net 30 Gün" >Net 30 Gün</option>
-                    </select>
-                  </label>
-                </div>
-              </div>
-
-              <div className="formsectionRow">
-                <h3>Öğe Listesi</h3>
-                <div className="itemFeatures">
-                  <div className="featuresItem">
-                    <p>Öğe Adı</p>
-                    <p>Ad</p>
-                    <p>Fiyat</p>
-                    <p>Toplam</p>
-                  </div>
-                  {
-                    itemList && itemList.map((x, i) =>
-                      <div className="featuresInputItem">
-                        <input type={x.text} />
-                        <input type={x.number} />
-                        <input type={x.number} />
-                        <p></p>
-                        <button type="button" onClick={() => setSelectedIndex(x.id)} ><Image src={"/images/cop.png"} width={12} height={16} /></button>
-                      </div>)
-                  }
-
-                  <button className="addnewItemBtn" type="button" onClick={() => setAddItem(prev => prev + 1)}>+ Yeni Öğe Ekle</button>
-                </div>
-              </div>
-
-            </div>
-            <div className="formBtnList">
-              <button type="button" onClick={() => close()}>Vazgeç</button>
-              <div className="saveBtns">
-                <button type="button">Taslak Olarak Kaydet</button>
-                <button>Kaydet ve Gönder</button>
               </div>
             </div>
-          </form>
-        </div>
-      </dialog>
+
+            <div className="formsectionRow">
+              <h4>Bill To</h4>
+              <label htmlFor="clientsname">Müşterinin Adı
+                <input type="text" name="clientsname" defaultValue={"19 Union Terrace"} />
+              </label>
+              <label htmlFor="clientemail">Müşterinin Epostası
+                <input type="text" name="clientemail" defaultValue={"London"} />
+              </label>
+              <label htmlFor="streetadress">Sokak Adresi
+                <input type="text" name="streetadress" defaultValue={"E1 3EZ"} />
+              </label>
+              <div className="citypostcountry">
+                <label htmlFor="city">Şehir
+                  <input type="text" name="citypostcountry" defaultValue={"London"} />
+                </label>
+                <label htmlFor="postcode">Posta Kodu
+                  <input type="text" name="postcode" defaultValue={"E1 3EZ"} />
+                </label>
+                <label htmlFor="country">Ülke
+                  <input type="text" name="country" defaultValue={"United Kingdom"} />
+                </label>
+              </div>
+              <div className="dateterms">
+                <label htmlFor="invoicedate">
+                  <input type="date" name="invoicedate" />
+                </label>
+                <label htmlFor="invoiceterm">
+                  <select name="invoiceterm">
+                    <option value="Net 1 Gün" selected>Net 1 Gün</option>
+                    <option value="Net 7 Gün" >Net 7 Gün</option>
+                    <option value="Net 14 Gün" >Net 14 Gün</option>
+                    <option value="Net 30 Gün" >Net 30 Gün</option>
+                  </select>
+                </label>
+              </div>
+            </div>
+
+            <div className="formsectionRow">
+              <h3>Öğe Listesi</h3>
+              <div className="itemFeatures">
+                <div className="featuresItem">
+                  <p>Öğe Adı</p>
+                  <p>Ad</p>
+                  <p>Fiyat</p>
+                  <p>Toplam</p>
+                </div>
+                {
+                  itemList && itemList.map((x, i) =>
+                    <div className="featuresInputItem">
+                      <input type={x.text} />
+                      <input type={x.number} />
+                      <input type={x.number} />
+                      <p></p>
+                      <button type="button" onClick={() => setSelectedIndex(x.id)} ><Image src={"/images/cop.png"} width={12} height={16} /></button>
+                    </div>)
+                }
+
+                <button className="addnewItemBtn" type="button" onClick={() => setAddItem(prev => prev + 1)}>+ Yeni Öğe Ekle</button>
+              </div>
+            </div>
+
+          </div>
+          <div className="formBtnList" style={{
+            display: show ? "flex" : "none"
+          }}>
+            <button type="button" onClick={() => setShow(false)}>Vazgeç</button>
+            <div className="saveBtns">
+              <button type="button">Taslak Olarak Kaydet</button>
+              <button>Kaydet ve Gönder</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
