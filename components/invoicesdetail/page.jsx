4b3568f8dc2bfe945acd77d0b5bbc2f484d49264
kemalozlp <ozlpkemal@gmail.com>
2024-10-20 18:13:54 +0300

    hata düzeltildi

---
+++ components/invoicesdetail/page.jsx
@@ -0,0 +1,21 @@
+import EditInvoices from "../editinvoices/editinvoices"
+import "./invoicesdetail.css"
+
+export default function InvoicesDetail() {
+
+  return (
+    <div className="invoiceDetail">
+      <div className="invoicedetailheader">
+        <p>Status</p>
+        <li>Pending</li>
+      </div>
+      <div className="invoicedetailbtn">
+        <EditInvoices />
+        <button className="dlt">Sil</button>
+        <button className="save">Ödendi olarak işaretle</button>
+      </div>
+    </div>
+
+   
+  )
+}
